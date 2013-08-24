12f53f8db5e120664b5af5631c91e309d11e1801
WooGenius <woogenius@nhnnext.org>
2013-08-24 16:35:11 +0900

    commit 3 roll class getSymbol method 에 조건문 추가

--- src/bowling/Roll.java
+++ src/bowling/Roll.java
@@ -4,28 +4,52 @@ import junit.framework.TestCase;
 
 public class Roll extends TestCase {
 	boolean isFirst;
-	int numOfPin;
-	int score;
+	boolean hitted = false;
+	int restPin;
+	Integer score;
 	char symbol;
 
-	public Roll(boolean isFirst, int numOfPin) {
+	public Roll(boolean isFirst, int restPin) {
 		this.isFirst = isFirst;
-		this.numOfPin = numOfPin;
+		this.restPin = restPin;
 	}
 
 	public void hit(int hittedPin) {
+		hitted = true;
 		score = hittedPin;
+		restPin -= hittedPin;
 	}
 
 	public char getSymbol() {
 		if (isStrike()) {
 			symbol = 'X';
+		} else if (isSpare()) {
+			symbol = '/';
+		} else if (isGutter()) {
+			symbol = '-';
+		} else if (notHitted()) {
+			symbol = '.';
+		} else {
+			symbol = Character.forDigit(score, 10);
 		}
+		
 		return symbol;
 	}
 
+	private boolean notHitted() {
+		return hitted==false;
+	}
+
+	private boolean isGutter() {
+		return ((hitted==true) && (score==0));
+	}
+
+	private boolean isSpare() {
+		return ((isFirst==false) && (restPin==0));
+	}
+
 	private boolean isStrike() {
-		return (isFirst==true) && (score==10);
+		return ((isFirst==true) && (restPin==0));
 	}
 
 }
--- test/bowling/RollTest.java
+++ test/bowling/RollTest.java
@@ -9,4 +9,26 @@ public class RollTest extends TestCase {
 		assertEquals('X', roll1st.getSymbol());
 	}
 	
+	public void testSpare() throws Exception {
+		Roll roll2nd = new Roll(false, 5);
+		roll2nd.hit(5);
+		assertEquals('/', roll2nd.getSymbol());
+	}
+	
+	public void testGutter() throws Exception {
+		Roll roll3rd = new Roll(false, 5);
+		roll3rd.hit(0);
+		assertEquals('-', roll3rd.getSymbol());
+	}
+	
+	public void testScore() throws Exception {
+		Roll roll4th = new Roll(true, 10);
+		roll4th.hit(5);
+		assertEquals('5', roll4th.getSymbol());
+	}
+	
+	public void testNotHitted() throws Exception {
+		Roll roll5th = new Roll(true, 10);
+		assertEquals('.', roll5th.getSymbol());
+	}
 }

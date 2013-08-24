a59aa062de4070afbfb1b8904ed879ef753718cd
WooGenius <woogenius@nhnnext.org>
2013-08-24 14:45:33 +0900

    test git

--- test/bowling/FrameTest.java
+++ test/bowling/FrameTest.java
@@ -7,7 +7,5 @@ public class FrameTest extends TestCase {
 		Frame frame1st = new Frame();
 		frame1st.pitch(9);
 		assertEquals(9, frame1st.getScore());
-		frame1st.pitch(1);
-		assertEquals(10, frame1st.getScore());
 	}
 }
--- test/bowling/PlayerTest.java
+++ test/bowling/PlayerTest.java
@@ -3,8 +3,4 @@ package bowling;
 import junit.framework.TestCase;
 
 public class PlayerTest extends TestCase {
-	public void testCreate() throws Exception {
-		Player player1 = new Player();
-		
-	}
 }
